d98bb86dc2272dfdd19c76542631e5a7b94ffd6e
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-07 02:22:55 +0200

    Fix(js): added to promises check to positive number

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,21 +8,25 @@ function onSubmitForm(evt) {
   evt.preventDefault();
   const { delay, step, amount } = evt.currentTarget.elements;
 
-  for (let i = 0; i < amount.value; i++) {
-    let position = i + 1;
-    const delays = Number(delay.value) + step.value * i;
+  if (delay.value < 0 || step.value < 0 || amount.value < 0) {
+    Notiflix.Notify.warning(`❗ Please enter a positive number`);
+  } else {
+    for (let i = 0; i < amount.value; i++) {
+      let position = i + 1;
+      const delays = Number(delay.value) + step.value * i;
 
-    createPromise(position, delays)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
-      });
+      createPromise(position, delays)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
   }
 
   evt.currentTarget.reset();
